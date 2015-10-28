86d529a103cfaa7496ace49e36a074915438d532
sammita94 <sammitachakravarti@yahoo.com>
2015-10-29 00:34:04 +0530

    data driven pages,slugify

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -4,5 +4,9 @@ from django.contrib import admin
 
 from rango.models import Category, Page
 
-admin.site.register(Category)
+# Add in this class to customized the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category,CategoryAdmin)
 admin.site.register(Page)
